3e39452726804f5f96aafece5750ae92da0943ce
zainammar <zainammar74@gmail.com>
2026-06-09 14:47:40 +0500

    Start Login

--- accounts/urls.py
+++ accounts/urls.py
@@ -23,3 +23,22 @@ urlpatterns = [
         name='profile'
     ),
 ]
+
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import profile
+
+urlpatterns = [
+    path('', auth_views.LoginView.as_view(
+        template_name='accounts/login.html'
+    ), name='home'),
+
+    path('login/', auth_views.LoginView.as_view(
+        template_name='accounts/login.html'
+    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('profile/', profile, name='profile'),
+]
